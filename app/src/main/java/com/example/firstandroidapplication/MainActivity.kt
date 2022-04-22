e3748dd2b2a5e67ed9aa9cf0c82cecfd17cd0456
simona-myvi <simona@myvi.io>
2022-04-22 14:51:57 +0300

    Initial commit

---
+++ app/src/main/java/com/example/firstandroidapplication/MainActivity.kt
@@ -0,0 +1,25 @@
+package com.example.firstandroidapplication
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Toast
+import kotlinx.android.synthetic.main.activity_main.*
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+var inputName = personNameTextId.text
+confirmButtonId.setOnClickListener {
+
+    if(inputName.isEmpty())
+    {
+        Toast.makeText(this, "Please, enter a name", Toast.LENGTH_LONG).show()
+    }
+    else
+    {
+        Toast.makeText(this, "Hi, $inputName", Toast.LENGTH_LONG).show()
+    }
+
+}}
+}
